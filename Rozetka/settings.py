1eb047d6019b908b65a2b9443e0908386f1a92a9
Vitaliy <sprinter-100m@ukr.net>
2023-05-10 16:17:02 +0300

    add MySQL

--- Rozetka/settings.py
+++ Rozetka/settings.py
@@ -79,8 +79,16 @@ WSGI_APPLICATION = 'Rozetka.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Create_MySQL',
+        'USER': 'root',
+        'PASSWORD': '1080Zxc',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+        },
+        'CONN_MAX_AGE': 300,
     }
 }
 
