29497a518a2084fbc34abbdd8f755e2aa1dcd9e0
Vitaliy <sprinter-100m@ukr.net>
2023-05-27 15:40:02 +0300

    add Multilingual

--- Rozetka/urls.py
+++ Rozetka/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.i18n import i18n_patterns
 
 from django.conf.urls.static import static
 from django.conf import settings
@@ -10,6 +11,12 @@ urlpatterns = [
     path('', include('main.urls')),
     path('accounts/', include('allauth.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns += i18n_patterns(
+    path('', include('main.urls')),
+    path('i18n/', include('django.conf.urls.i18n')),
+    prefix_default_language=False
+)
 
 if settings.DEBUG:
 	urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
