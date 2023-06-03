676381e1e2032630c0db5b113dc2be93a04447e9
Vitaliy <sprinter-100m@ukr.net>
2023-06-03 09:56:03 +0300

    add personal_cabinet

--- personal_cabinet/urls.py
+++ personal_cabinet/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from .import views
 
-app_name = 'cabinet'
+
+app_name = 'personal_cabinet'
 
 urlpatterns = [
-    path('', views.PersonalCabinetView.as_view(), name='personal_cabinet'),
+    path('', views.PersonalCabinetView.as_view(), name='cabinet'),
+    path('orders/', views.OrderListView.as_view(), name='order_list'),
+    path('wishlist/', views.WishlistView.as_view(), name='wish_list'),
 ]
--- personal_cabinet/views.py
+++ personal_cabinet/views.py
@@ -5,4 +5,28 @@ from .models import UserProfile
 
 class PersonalCabinetView(ListView):
     model = UserProfile
-    template_name = 'personal_cabinet/personal_cabinet.html'
+    context_object_name = 'user_profile'
+
+    def get_queryset(self):
+        return UserProfile.objects.filter(user=self.request.user)
+    
+
+class OrderListView(ListView):
+    model = UserProfile
+    template_name = 'personal_cabinet/order_list.html'
+    context_object_name = 'orders'
+
+    def get_queryset(self):
+        user_profile = self.request.user.userprofile
+        return user_profile.orders
+    
+
+class WishlistView(ListView):
+    model = UserProfile
+    template_name = 'personal_cabinet/wish_list.html'
+    context_object_name = 'wishlist_items'
+
+    def get_queryset(self):
+        user_profile = self.request.user.userprofile
+        return user_profile.wishlist
+    
